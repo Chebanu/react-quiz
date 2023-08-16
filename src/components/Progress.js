8b2a6849f2bef1853a6c8acd6e248251edd7f167
Alexei <0alexchebanu@gmail.com>
2023-08-16 23:31:28 +0300

    Quiz

---
+++ src/components/Progress.js
@@ -0,0 +1,17 @@
+function Progress({ index, numQuestions, points, maxPossiblePoints, answer }) {
+  return (
+    <header className="progress">
+      <progress max={numQuestions} value={index + Number(answer !== null)} />
+
+      <p>
+        Question <string>{index + 1}</string>/ {numQuestions}
+      </p>
+
+      <p>
+        <strong>{points}</strong> / {maxPossiblePoints}
+      </p>
+    </header>
+  );
+}
+
+export default Progress;
